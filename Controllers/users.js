b639c43b71cc22c18c9cb9cf3b8a60c055a97eb7
Jayanth9710 <kumarjayanth55@gmail.com>
2021-12-30 18:58:19 +0530

    login and registrantion method changed

--- Controllers/users.js
+++ Controllers/users.js
@@ -4,79 +4,81 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const mongodb = require ("mongodb");
 const User = require('../models/user');
+const mongoClient = mongodb.MongoClient;
 
 dotenv.config();
+const url = process.env.MONGO_URI;
 
-// Registering Users.
- const registerUser =  (req, res) => {
-    const {name,username,email,password,shipAdd,crrAdd} =req.body;
-      User.findOne({email:email})
-    .then((savedUser)=>{
-      if (savedUser) {
-        res.status(403).json({
-          message: "Email is already registered.",
+// // Registering Users.
+//  const registerUser =  (req, res) => {
+//     const {name,username,email,password,shipAdd,crrAdd} =req.body;
+//       User.findOne({email:email})
+//     .then((savedUser)=>{
+//       if (savedUser) {
+//         res.status(403).json({
+//           message: "Email is already registered.",
           
-        });
-      } else {
-        // Hash the password
-        let salt =  bcrypt.genSaltSync(10);
-        let hash =  bcrypt.hashSync(password, salt);
+//         });
+//       } else {
+//         // Hash the password
+//         let salt =  bcrypt.genSaltSync(10);
+//         let hash =  bcrypt.hashSync(password, salt);
         
   
-        // Confirm registration.
-        const newUser = new User({ 
-          username:username,
-          email:email,
-          name:name,
-          shippingAddress:shipAdd,
-          currentAddress:crrAdd,
-          password:hash
-        })
+//         // Confirm registration.
+//         const newUser = new User({ 
+//           username:username,
+//           email:email,
+//           name:name,
+//           shippingAddress:shipAdd,
+//           currentAddress:crrAdd,
+//           password:hash
+//         })
   
-        // Save user and send Response.
-          newUser.save();
+//         // Save user and send Response.
+//           newUser.save();
   
-        res.json({
-          message: "User registered successfully.",
-        });
-      }
+//         res.json({
+//           message: "User registered successfully.",
+//         });
+//       }
 
-    })
-  .catch(err=>{
-    console.log(err)
-  })
-}
+//     })
+//   .catch(err=>{
+//     console.log(err)
+//   })
+// }
   
 
-// Login.
+// // Login.
 
- const loginUser = async (req, res) => {
-  try {
-    let user = await User.findOne({ email: req.body.email });
+//  const loginUser = async (req, res) => {
+//   try {
+//     let user = await User.findOne({ email: req.body.email });
 
-    if (user) {
-      let validPassword = bcrypt.compareSync(req.body.password, user.password);
-      if (validPassword) {
-        // Generate JWT token
-        let token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-        res.json({
-          message: user._id,
-          token,
-        });
-      } else {
-        res.status(403).json({
-            message: "Username/Password is incorrect"
-        })
-      }
-    } else {
-        res.status(403).json({
-            message: "Email is already registered.",
-          });
-    }
-  } catch (error) {
-    res.status(500).json(error);
-  }
-};
+//     if (user) {
+//       let validPassword = bcrypt.compareSync(req.body.password, user.password);
+//       if (validPassword) {
+//         // Generate JWT token
+//         let token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+//         res.json({
+//           message: user._id,
+//           token,
+//         });
+//       } else {
+//         res.status(403).json({
+//             message: "Username/Password is incorrect"
+//         })
+//       }
+//     } else {
+//         res.status(403).json({
+//             message: "Email is already registered.",
+//           });
+//     }
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// };
 
 //--------------------------- Get User Address---------//
 
@@ -154,6 +156,91 @@ const editUserAddress = async (req,res) => {
         
 //     }
 // }
+
+//------------Registering Users---------//
+
+const registerUser = async (req,res) => {
+  req.body.role="user";
+  try {
+      // Connecting to DB
+      let client = await mongoClient.connect(url);
+
+      //Select the DB
+let db = client.db("ecommerce");
+let emailCheck = await db.collection("users").findOne({ email : req.body.email });
+
+if (!emailCheck) {
+  // Hashing the Password 
+  let salt = bcrypt.genSaltSync(10);
+  let hash = bcrypt.hashSync(req.body.password,salt);
+
+  req.body.password = hash;
+
+  //Posting the User details to DB
+
+  let data = await db.collection('users').insertOne(req.body);
+
+  //Closing the connection to DB
+  await client.close();
+
+  res.json({
+      message:"User Registered Successfully"
+  });
+} else {
+  // Case when email is already present in DB.
+  res.status(400).json({
+      message:"E-mail is already registered.Please try with different e-mail ID."
+  });
+}
+  } catch (error) {
+      console.log(error);
+      res.status(500).json({
+          message:"Registration Failed due to some reason or Myabe check your connection."
+      })
+  }
+}
+
+//-------------User Login----------------//
+
+const loginUser = async (req,res) => {
+  try {
+      // Connecting to DB
+      let client = await mongoClient.connect(url);
+
+      // Selecting the DB
+      let db = client.db("ecommerce");
+
+      //Finding the user details in DB
+
+      let user = await db.collection("users").findOne({ email : req.body.email });
+
+      if (user) {
+          let validPassword = bcrypt.compareSync(req.body.password,user.password);
+
+          if(validPassword) {
+              let token = jwt.sign({ id : user._id},process.env.JWT_SECRET);
+          res.json({
+              message:true,
+              token,
+              user:user._id
+          });
+          } else {
+              res.status(500).json({
+                  message:"Username/Password is incorrect",
+              });
+          }
+      } else {
+          res.status(500).json({
+              message:"Username/Password is incorrect",
+          });
+      }
+  } catch (error) {
+      console.log(error)
+      res.status(500).json({
+          message:"Sign in failed due to some reason or maybe check your connection."
+      })
+  }
+}
 
 module.exports ={
   registerUser,
